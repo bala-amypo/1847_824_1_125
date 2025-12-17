122a105eda7ebd268ec73ba0daf8e3b42ec40bbc
bala-amypo <bala@amypo.in>
2025-12-17 06:32:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingrediententity.java
+++ src/main/java/com/example/demo/entity/Ingrediententity.java
@@ -2,7 +2,17 @@ package com.example.demo.entity;
 
 
 @entity
-public class  Ingrediententity
-{
-    
+public class  Ingrediententity{
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+
+@Column(unique=true)
+private String name;
+
+private String unit;
+private BigDecimal costperUnit;
+private Boolean active;
+private Timestamp crea
 }
+
