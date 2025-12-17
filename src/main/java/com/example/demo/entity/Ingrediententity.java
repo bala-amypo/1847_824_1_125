53d70b34356acef003275877ad534773811ca76a
bala-amypo <bala@amypo.in>
2025-12-17 08:06:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingrediententity.java
+++ src/main/java/com/example/demo/entity/Ingrediententity.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
-
+import jakarta.precision.Entity;
+import jakarta.precision.GeneratedValue;
+import jakarta.precision.GenerationType;
+import jakarta.precision.Id;
+import ja
 @Entity
 public class  Ingrediententity{
 @Id
@@ -31,6 +35,62 @@ protected void onCreate(){
 protected void onUpdate(){
     updateAt = LocalDateTime.now();
 }
+
+public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public BigDecimal getCostperUnit() {
+        return costperUnit;
+    }
+
+    public void setCostperUnit(BigDecimal costperUnit) {
+        this.costperUnit = costperUnit;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(LocalDateTime update) {
+        this.update = update;
+    }
 
 
 }
