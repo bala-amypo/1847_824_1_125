a5f9cc58f89b1ce2828886d70d9a6d679665101d
bala-amypo <bala@amypo.in>
2025-12-18 09:07:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Userentity.java
+++ src/main/java/com/example/demo/entity/Userentity.java
@@ -1,33 +1,90 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
 
 @Entity
-public class  Userentity{
+public class Userentity {
 
     @Id
-    @GeneratedValue(stratgy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String fullname;
 
-    @Column(unique=true)
-    private String  email;
-    
+    @Column(unique = true)
+    private String email;
+
     private String active;
     private String role;
-    
+
     @CreationTimestamp
     private Timestamp createdAt;
 
-    @PerPersisit
-    public void onCreate(){
-    this createAt=new Timestamp(System.currentTimeMillis());
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+   
+    public Userentity() {
     }
 
     
 
-   
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
