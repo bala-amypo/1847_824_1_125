1ee5c5385e4669ad0f266c5b62256b69c58f1076
bala-amypo <bala@amypo.in>
2025-12-18 06:46:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Categoryentity.java
+++ src/main/java/com/example/demo/entity/Categoryentity.java
@@ -1,11 +1,21 @@
 package com.example.demo.entity;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.math.
 @Entity
 public class Categoryentity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long 
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+    private Boolean active;
+
 
 
 
