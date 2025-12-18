ee5ff5e892a4699875bab4ea57c5f2f446be2ed4
bala-amypo <bala@amypo.in>
2025-12-18 14:40:34 +0000

    User commit

--- src/main/java/com/example/demo/service/MenuItemservice.java
+++ src/main/java/com/example/demo/service/MenuItemservice.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 import jakarta.persistence.service;
-import com.example.demo.entity.MenuItemtentity;
+import com.example.demo.entity.MenuItementity;
 @Service
 public interface MenuItemservice{
     public MenuItem createMenuItem(MenuItem item);
