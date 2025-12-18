f09fa26c16f018701a9ac687431bbda21e41c709
bala-amypo <bala@amypo.in>
2025-12-18 06:11:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Categoryentity.java
+++ src/main/java/com/example/demo/entity/Categoryentity.java
@@ -3,30 +3,41 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
-import java.lang.Boolean;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.math.BigDecimal;
-import org.hibernate.annotation.CreationTimestamp;
-import org.hibernate.annotation.UpdateTime;
 import java.sql.Timestamp;
 
 @Entity
-public class  Categoryentity{
+public class Categoryentity {
+
     @Id
-    @GenerationValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique =  true)
+    @Column(unique = true)
     private String name;
 
     private String description;
     private BigDecimal sellingPrice;
     private Boolean active;
+
     @CreationTimestamp
     private Timestamp createAt;
+
     @UpdateTimestamp
     private Timestamp updatedAt;
 
-    public Categoryentity(Boolean active, Timestamp createAt, String description, Long id, String name, BigDecimal sellingPrice, Timestamp updatedAt) {
+  
+   
+
+    public Categoryentity(Boolean active, Timestamp createAt, String description,
+                          Long id, String name, BigDecimal sellingPrice,
+                          Timestamp updatedAt) {
         this.active = active;
         this.createAt = createAt;
         this.description = description;
@@ -35,62 +46,7 @@ public class  Categoryentity{
         this.sellingPrice = sellingPrice;
         this.updatedAt = updatedAt;
     }
-    
-
-
-    public Long getId() {
-        return id;
+     public Categoryentity() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getSellingPrice() {
-        return sellingPrice;
-    }
-
-    public void setSellingPrice(BigDecimal sellingPrice) {
-        this.sellingPrice = sellingPrice;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public Timestamp getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Timestamp createAt) {
-        this.createAt = createAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-}
