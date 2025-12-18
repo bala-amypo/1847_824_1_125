7765e2922caf2bd7a8e05372e747642aa2e5fb3b
bala-amypo <bala@amypo.in>
2025-12-18 08:37:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/RecipeIngrediententity.java
+++ src/main/java/com/example/demo/entity/RecipeIngrediententity.java
@@ -1,16 +1,56 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
-public class  RecipeIngrediententity{
+public class RecipeIngrediententity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String menuItem;
     private String ingredient;
-    
+    private Double quantityRequired;
 
+  
+    public RecipeIngrediententity() {
+    }
 
-    
+   
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(String menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Double getQuantityRequired() {
+        return quantityRequired;
+    }
+
+    public void setQuantityRequired(Double quantityRequired) {
+        this.quantityRequired = quantityRequired;
+    }
 }
