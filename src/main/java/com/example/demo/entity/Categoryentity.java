614ca54844e1e6578863bd816cfc539fd1f5e92d
bala-amypo <bala@amypo.in>
2025-12-18 05:09:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/Categoryentity.java
+++ src/main/java/com/example/demo/entity/Categoryentity.java
@@ -18,6 +18,69 @@ public class  Categoryentity{
     private Timestamp createAt;
     private Timestamp updatedAt;
 
+    public Categoryentity(Boolean active, Timestamp createAt, String description, Long id, String name, BigDecimal sellingPrice, Timestamp updatedAt) {
+        this.active = active;
+        this.createAt = createAt;
+        this.description = description;
+        this.id = id;
+        this.name = name;
+        this.sellingPrice = sellingPrice;
+        this.updatedAt = updatedAt;
+    }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(BigDecimal sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Timestamp createAt) {
+        this.createAt = createAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
