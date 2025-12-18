7006abb93d14d1b792e88dc2289334a70595a188
bala-amypo <bala@amypo.in>
2025-12-18 14:19:34 +0000

    User commit

--- src/main/java/com/example/demo/service/ProfitCalculation.java
+++ src/main/java/com/example/demo/service/ProfitCalculation.java
@@ -2,9 +2,11 @@ package.com.example.demo.service;
 import jakarta.persistence.service;
 import com.example.demo.entity.ProfitCalculationentity;
 
+@service
 public ProfitCalculationservice{
     public ProfitCalculation calculateProfit(Long menuItemId);
     public ProfitCalculation updateRecipeIngredient(Long id,Double quantity);
     public ProfitCalculation getIngredientFromRecipe(Long menuItemId);
-    public ProfitCalculation removeIngredientFromRecipe(Long id)
+    public ProfitCalculation removeIngredientFromRecipe(Long id);
+    public ProfitCalculation getTotalQuantityOfIngredient(Long ingredientId);
 }
---
+++ src/main/java/com/example/demo/service/RecipeIngredientRecordservice.java
@@ -0,0 +1,8 @@
+package.com.example.demo.service;
+import jakarta.persistence.service;
+import com.example.demo.entity.RecipeIngrediententity;
+
+@service
+public ProfitCalculationservice{
+
+}
