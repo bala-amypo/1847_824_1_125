924949c53f01038299cb370ffa0e2fecb70a1cf2
bala-amypo <bala@amypo.in>
2025-12-18 09:52:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -2,6 +2,17 @@ package com.example.demo.repository;
 
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.MenuItementity;
+public interface MenuItemRepository extends JpaRepository<MenuItementity,Long>{
+
+}
+
+
+
+
+
 public class MenuItemRepository
 {
     
