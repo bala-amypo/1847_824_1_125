fe1f6182d19257e0b0c1f0ad6ddd601f88295cce
bala-amypo <bala@amypo.in>
2025-12-18 07:47:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/Categoryentity.java
+++ src/main/java/com/example/demo/entity/Categoryentity.java
@@ -1,12 +1,14 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import java.lang.Boolean;
 import jakarta.persistence.Column;
+
 @Entity
 public class Categoryentity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,12 +19,40 @@ public class Categoryentity {
     private String description;
     private Boolean active;
 
-    
-
-
-
-
-
+    public Categoryentity() {
+    }
 
    
+
+    public Long getId() {
+        return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
