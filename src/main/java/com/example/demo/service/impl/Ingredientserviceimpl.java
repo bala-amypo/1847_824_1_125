cf4ebf0328ebb93d00e15840fc50e72eb21e903d
bala-amypo <bala@amypo.in>
2025-12-19 08:30:57 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Ingredientserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Ingredientserviceimpl.java
@@ -2,5 +2,12 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.Ingredient;
 import com.example.demo.repository.IngredientRepository;
-import com.example.demo.service.Ingredientserviceimpl;
-import org.springframework.stereotype.
+import com.example.demo.service.Ingredientservice;
+import org.springframework.stereotype.service;
+import java.util.List;
+
+@Service
+public class Ingredientserviceimpl implements Ingredientservice{
+
+    private final IngredientRepository ingredientR
+}
