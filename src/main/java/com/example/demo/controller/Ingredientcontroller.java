a8469f1aaaa66aed31a6873ac363d2bf4dd4ff06
bala-amypo <bala@amypo.in>
2025-12-19 08:39:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/Ingredientcontroller.java
+++ src/main/java/com/example/demo/controller/Ingredientcontroller.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.entity.Ingrediententity;
+import com.example.demo.service.Ingredientservice;
+import 
+
 
 public class  Ingredientcontroller
 {
