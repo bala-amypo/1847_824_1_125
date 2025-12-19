f0f3a663ee75988dbb82ea8e84b342f329973cc9
bala-amypo <bala@amypo.in>
2025-12-19 04:02:53 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Categoryserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Categoryserviceimpl.java
@@ -2,4 +2,9 @@ import com.example.demo.service.impl;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.entity.Categoryentity;
 import com.example.demo.service.Categoryservice;
-import org.springframework.sterotype
+import org.springframework.stereotype.service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public cal
