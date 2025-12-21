c29f9e06354fa54033cfdb8f28e7007691737ebd
bala-amypo <bala@amypo.in>
2025-12-21 06:03:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/IngredientRepository.java
+++ src/main/java/com/example/demo/repository/IngredientRepository.java
@@ -5,6 +5,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.Ingrediententity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository
-        extends JpaRepository<Ingrediententity, Long> {
+public interface IngredientRepository extends JpaRepository<Ingrediententity, Long> {
 }
--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MenuItemRepositrory
-        extends JpaRepository<MenuItementity, Long> {
+public interface MenuItemRepositrory extends JpaRepository<MenuItementity, Long> {
 
     Optional<MenuItementity> findByName(String name);
 }
