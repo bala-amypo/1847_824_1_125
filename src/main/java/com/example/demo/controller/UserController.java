44d78641d5841f2ca130b95eb496dc76a9f27718
bala-amypo <bala@amypo.in>
2025-12-23 04:43:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,35 +1,35 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
 
 
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.RegisterRequest;
-import com.example.demo.entity.UserEntity;
-import com.example.demo.service.UserService;
+// import com.example.demo.dto.AuthRequest;
+// import com.example.demo.dto.RegisterRequest;
+// import com.example.demo.entity.UserEntity;
+// import com.example.demo.service.UserService;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 
 
-@RestController
-@RequestMapping("/auth")
-public class UserController {
+// @RestController
+// @RequestMapping("/auth")
+// public class UserController {
 
-    private final UserService service;
+//     private final UserService service;
 
-    public UserController(UserService service) {
-        this.service = service;
-    }
+//     public UserController(UserService service) {
+//         this.service = service;
+//     }
 
-    @PostMapping("/register")
-    public UserEntity register(@RequestBody RegisterRequest request) {
-        return service.register(request);
-    }
+//     @PostMapping("/register")
+//     public UserEntity register(@RequestBody RegisterRequest request) {
+//         return service.register(request);
+//     }
 
-    @PostMapping("/login")
-    public UserEntity login(@RequestBody AuthRequest request) {
-        return service.login(request);
-    }
-}
+//     @PostMapping("/login")
+//     public UserEntity login(@RequestBody AuthRequest request) {
+//         return service.login(request);
+//     }
+// }
