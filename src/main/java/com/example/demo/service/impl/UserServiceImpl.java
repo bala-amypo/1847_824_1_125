85cbdd74aa71d5019cf9d459053e0400ab8e4a65
bala-amypo <bala@amypo.in>
2025-12-24 05:24:21 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,49 +1,49 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import com.example.demo.dto.AuthRequest;
-// import com.example.demo.dto.RegisterRequest;
-// import com.example.demo.entity.UserEntity;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
-// import org.springframework.stereotype.Service;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
-//     private final UserRepository repository;
+    private final UserRepository repository;
 
-//     public UserServiceImpl(UserRepository repository) {
-//         this.repository = repository;
-//     }
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
 
-//     @Override
-//     public UserEntity register(RegisterRequest request) {
+    @Override
+    public UserEntity register(RegisterRequest request) {
 
-//         if (repository.existsByEmail(request.getEmail())) {
-//             throw new RuntimeException("Email already exists");
-//         }
+        if (repository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
 
-//         UserEntity user = new UserEntity();
-//         user.setFullName(request.getFullName());
-//         user.setEmail(request.getEmail());
+        UserEntity user = new UserEntity();
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
 
-//         user.setPassword(request.getPassword());
+        user.setPassword(request.getPassword());
 
-//         user.setRole(request.getRole());
+        user.setRole(request.getRole());
 
-//         return repository.save(user);
-//     }
+        return repository.save(user);
+    }
 
-//     @Override
-//     public UserEntity login(AuthRequest request) {
+    @Override
+    public UserEntity login(AuthRequest request) {
 
-//         UserEntity user = repository.findByEmail(request.getEmail())
-//                 .orElseThrow(() -> new RuntimeException("Invalid email"));
+        UserEntity user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email"));
 
-//         if (!user.getPassword().equals(request.getPassword())) {
-//             throw new RuntimeException("Invalid password");
-//         }
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
 
-//         return user;
-//     }
-// }
+        return user;
+    }
+}
