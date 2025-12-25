1592b8610853499447a7f4487c9b20163dfdd517
bala-amypo <bala@amypo.in>
2025-12-25 12:58:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -16,11 +16,25 @@
 
 
 
+// package com.example.demo.repository;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface UserRepository extends JpaRepository<Object, Long> {
+//     boolean existsByEmail(String email);
+// }
+
 package com.example.demo.repository;
 
+import com.example.demo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Object, Long> {
-    boolean existsByEmail(String email);
-}
+import java.util.Optional;
 
+public interface UserRepository
+        extends JpaRepository<UserEntity, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByEmail(String email);
+}
