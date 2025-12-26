a78afcfbb0e2f5070da70ba4770acf53593f6e6b
bala-amypo <bala@amypo.in>
2025-12-26 18:44:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -46,3 +46,82 @@ public class AuthController {
         return ResponseEntity.ok(new AuthResponse(token));
     }
 }
+ package com.example.demo1.Controller; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo1.Entity.ExtraStudent;
+import com.example.demo1.Security.jwtutil;
+import com.example.demo1.Service.ExtraStudentService;
+import com.example.demo1.dto.AuthRequest;
+import com.example.demo1.dto.AuthResponse;
+
+// import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+   
+
+    @PostMapping("/register")
+    public ExtraStudent register(@RequestBody ExtraStudent stu) {
+        return ser.saveExtraStudent(stu);
+    }
+
+
+//  @PostMapping("/login")
+//     public String login(@RequestBody AuthRequest request) {
+//         ExtraStudent student = ser.CheckEmail(request.getEmail());
+//         if (student == null) {
+//             throw new RuntimeException("User Not Found");
+
+//         }
+//         if (!encoder.matches(request.getPassword(), student.getPassword())) {
+//             throw new RuntimeException("Invalid credentials");
+//         }
+
+//         return "Login successful for " + student.getEmail();
+// }
+@Autowired
+    ExtraStudentService ser;
+    @Autowired
+    PasswordEncoder encoder;
+
+
+    @Autowired jwtutil pd;
+
+    @PostMapping("/add")
+    public ExtraStudent addExtraStudent(@RequestBody ExtraStudent stu) {
+        return ser.saveExtraStudent(stu);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        ExtraStudent student = ser.CheckEmail(request.getEmail());
+        if (student == null) {
+            throw new RuntimeException("User Not Found");
+
+        }
+        if (!encoder.matches(request.getPassword(), student.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        // return "Login successful for " + student.getEmail();
+
+
+        String token = pd.generateToken(
+                student.getEmail(),
+                student.getRole()
+        );
+
+
+        return new AuthResponse(token, student.getRole());
+
+    }
+}
