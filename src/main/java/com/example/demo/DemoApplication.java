a1535698fbe5d823e2c14dd29e39e6083aceedbc
bala-amypo <bala@amypo.in>
2025-12-27 04:40:54 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,10 +7,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println(new BCryptPasswordEncoder().encode("1234"));
+        System.out.println(new BCryptPasswordEncoder().encode("123"));
 
 		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
-		if(encoder.matches("1234","$2a$10$BknWbUTkOkMC3YINo9ZLsO2p19EMsnPi3Jye4fg8H55qYRNGCsCEy")){
+		if(encoder.matches("123","$2a$10$u0V2AUT48MHQjtXiWyB.U.eUln6AovVtB7pKeJLRba7J1FATeKoh2")){
 			System.out.println("password matched");
 		}else{
 			System.out.println("no match");
