fed09f32a2423a19aca654264f279c81585e5035
bala-amypo <bala@amypo.in>
2025-12-27 04:48:00 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-       
-         
+    
     }
 
 }
