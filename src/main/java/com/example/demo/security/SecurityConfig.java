b73b0fe769ffcfaaa0b94a3c8513fff816c2f5b4
bala-amypo <bala@amypo.in>
2025-12-28 06:09:43 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -23,12 +23,13 @@
 // }
 
 
-
 package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -47,4 +48,11 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    // ✅ REQUIRED for UserServiceImpl
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
